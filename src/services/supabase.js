727c2dcb43375f198a62c69212d92827dc8ed413
AhmedMohammedKhorkhash <khorkhash3@gmail.com>
2023-11-29 11:33:30 +0200

    Implemet uploading images to supabase.

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,5 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = "https://gzgkrufshibppapsnrqh.supabase.co";
+
+export const supabaseUrl = "https://gzgkrufshibppapsnrqh.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd6Z2tydWZzaGlicHBhcHNucnFoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDA2NDA4MjQsImV4cCI6MjAxNjIxNjgyNH0.oFunm0eyMMOier6EPeJrNrXcALrM4WfqtmWj_bAlvOM";
 const supabase = createClient(supabaseUrl, supabaseKey);
